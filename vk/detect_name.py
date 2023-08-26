6018be2e796ffac37e6ca9ab29df14b0ceac2544
Danil <DADemidenko@stud.kantiana.ru>
2023-08-26 18:26:23 +0200

    Now detect user id/name

---
+++ vk/detect_name.py
@@ -0,0 +1,38 @@
+from roboflow import Roboflow
+import cv2 as cv
+import easyocr
+
+rf = Roboflow(api_key="fKNgN4TLK6ZSRlRHPdZZ")
+project = rf.workspace().project("hak2023_2")
+model = project.version(1).model
+
+
+nums = 2
+a = model.predict("/content/hak2023/Data/vk/photo_2023-08-26_13-41-23.jpg", confidence=40, overlap=30).json()
+
+def read_name_id_vk(data:dict, img_path) -> None:
+  print(data)
+  if not data['predictions']:
+    return
+  if data['predictions'][0]['confidence'] < 0.5:
+    print("uncertainty")
+    return
+  bbox = data['predictions'][0]
+  print(bbox['x'], bbox['x']+400)
+  img = cv.imread(img_path)
+  x = bbox['x'] - bbox['width']//2
+  to_x = bbox['x'] + bbox['width']//2
+  
+  y = bbox['y'] - bbox['height']//2
+  to_y = bbox['y'] + bbox['height']//2
+
+  cropped_img = img[y: to_y, x:to_x]
+  cv.imwrite("d.png", img)
+  cv.imwrite("screen.png", cropped_img)
+  
+  results = reader.readtext(cropped_img)
+  out = results[0][1]
+  return out
+
+
+read_name_id_vk(a, "/content/hak2023/Data/vk/photo_2023-08-26_13-41-23.jpg")
