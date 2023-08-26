06daf56380f795c1ab6f0b28e121449a786fad23
Danil <DADemidenko@stud.kantiana.ru>
2023-08-26 15:45:39 +0200

    Add vk data extractor
    Need to detect id

--- preproc.py
+++
@@ -1,8 +0,0 @@
-import cv2 as cv
-
-img = cv.imread(r"C:\Users\caretaker\Documents\hakaton\hak2023\Data\tg\images\0ac1d316-523f-4a67-8cb5-317ec4d4f41f.jpg")
-gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-gray = cv.resize(gray, (1280, 720))
-
-cv.imshow("gray", gray)
-cv.waitKey(0)
---
+++ vk.py
@@ -0,0 +1,75 @@
+import pandas as pd
+import numpy as np
+import pandas as pd
+import numpy as np
+
+from glob import glob
+import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
+
+import easyocr
+
+img_fns = glob('Data/vk/images/*')
+reader = easyocr.Reader(['en', 'ru'], gpu = True)
+REGEXP = r'^[1-9]*[ ]?[Дд]ру|[чн]ик[иа(ов)]*'
+
+num = 234
+
+def find(df, bbox:list) -> list:
+  """
+  Расширяем область поиска
+  """
+  bboxes = df.bbox.tolist()
+  text = df.text.tolist()
+  for i in range(len(bboxes)):
+   
+    if (-30 < bbox[0][0] - bboxes[i][0][0] < 40) and  (0 < bbox[0][1]-bboxes[i][0][1] < 100) and text[i].isdigit():
+      print(bbox[0][0] - bboxes[i][0][0])
+      return int(text[i])
+  else:
+    return None
+
+def convert_to_dict(string: str) -> None:
+  out = string.split() 
+  if len(out) == 2:
+    try:
+      out = {out[1]: int(out[0])}
+      return out
+    except:
+      pass
+
+    try:
+      out = {out[0]: int(out[1])}
+      return out
+    except:
+      return None
+    
+
+def get_data_vk(df, img_path):
+    use_to = pd.DataFrame(df['text'].str.lower())
+    use_to['bbox'] = df['bbox']
+    use_to = use_to.loc[use_to['text'].str.contains(REGEXP)]
+    tmp = use_to['text'].tolist()
+    bboxes = use_to['bbox'].tolist()
+    out = []
+    
+    for i in range(len(tmp)):
+      g = convert_to_dict(tmp[i])
+      if g:
+        out.append(g)
+      else:
+        check = find(df, bboxes[i])
+        if check:
+          out.append({tmp[i]:check})
+
+    return out
+
+if __name__ == "__main__":
+    img_path = img_fns[num]
+    results = reader.readtext(img_path)
+
+    df = pd.DataFrame(results, columns=['bbox','text','conf'])
+
+    res = get_data_vk(df, img_path)
+    res 
+
